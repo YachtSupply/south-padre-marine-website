f9091a1919c45d8353aa419d3a8b303e16b5c6b9
Andi Anderson <andi@sailplan.com>
2026-04-04 21:37:42 +0000

    chore: bump version to 1.1.18 [auto-version]

--- src/site.config.ts
+++ src/site.config.ts
@@ -6,7 +6,7 @@
 // for each deployed site.
 // ============================================================
 
-export const TEMPLATE_VERSION = '1.1.17';
+export const TEMPLATE_VERSION = '1.1.18';
 
 export type BoatworkEventType =
   | 'profile.updated'
